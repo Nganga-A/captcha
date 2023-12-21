9b131792b16b3fe1af30ef00348ef182e0ceda29
Abed Nganga <ngangaabed@gmail.com>
2023-12-21 13:27:53 +0300

    Mapping captcha images

--- src/app/components/Captcha.js
+++ src/app/components/Captcha.js
@@ -1,7 +1,18 @@
 export default function Captcha() {
+    const imageLocations = new Array(9).fill(null).map((value, index) => {
+        return `/api/captcha-image/${index}.png`;
+    });
+
     return (
-        <div>
-            <p>captcha goes here</p>
+        <div className="captcha">
+            <h2>Select all dogs: </h2>
+            <div className="captcha-images">
+                {imageLocations.map(imageUrl => (
+                    <div>
+                        <img src={imageUrl } alt=""/>
+                    </div>
+                ))}
+            </div>
         </div>
-    )
+    );
 }
